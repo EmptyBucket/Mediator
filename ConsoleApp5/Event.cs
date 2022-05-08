5019c38fd33a0e1f9bacc0e884c9e084a5cf904a
apolitov <zxfess@yandex.ru>
2022-05-08 05:21:03 +0400

    tmp

--- ConsoleApp5/Event.cs
+++ ConsoleApp5/Event.cs
@@ -1,3 +1,14 @@
+using FlexMediator.Handlers;
+using FlexMediator.Utils;
+
 namespace ConsoleApp5;
 
 public record Event(string Name);
+
+public class EventHandler : IHandler<Event, string>
+{
+    public Task<string> HandleAsync(Event message, MessageOptions options, CancellationToken token)
+    {
+        return Task.FromResult(message.Name);
+    }
+}
--- ConsoleApp5/Program.cs
+++ ConsoleApp5/Program.cs
@@ -1,22 +1,33 @@
 using ConsoleApp5;
-using EasyNetQ;
 using FlexMediator;
+using FlexMediator.Pipes;
 using Microsoft.Extensions.DependencyInjection;
-using JsonSerializer = System.Text.Json.JsonSerializer;
+using StackExchange.Redis;
+using EventHandler = ConsoleApp5.EventHandler;
 
 var serviceCollection = new ServiceCollection();
 serviceCollection
     .RegisterEasyNetQ("host=localhost")
     .AddMediator(async (f, p) =>
     {
-    })
-    .AddScoped<EventHandler>();
+        var connectionMultiplexer = ConnectionMultiplexer.Connect("localhost");
+        var subscriber = connectionMultiplexer.GetSubscriber();
+        
+        var redisMqPipe = new RedisMqPipe(subscriber);
+        await redisMqPipe.In<Event>(p);
+        await redisMqPipe.In<Event, string>(p);
+
+        var handlingPipe = f.Create<HandlingPipe>();
+        await handlingPipe.In<Event>(redisMqPipe);
+        await handlingPipe.In<Event, string>(redisMqPipe);
+
+        handlingPipe.Out<Event>(_ => new EventHandler());
+        handlingPipe.Out<Event, string>(_ => new EventHandler());
+    });
 var serviceProvider = serviceCollection.BuildServiceProvider();
 
 var mediator = serviceProvider.GetRequiredService<IMediator>();
 // await mediator.Publish(new Event("qwe"));
-// var result = await mediator.Send<Event, string>(new Event("qwe"));
+var result = await mediator.Send<Event, string>(new Event("qwe"));
 
-var bus = serviceProvider.GetService<IBus>();
-var serialize = JsonSerializer.Serialize(new Exception("asdasd"));
 await Task.Delay(10_000);
