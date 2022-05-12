7c55ab05d38788a424610217be244886756b958b
apolitov <zxfess@yandex.ru>
2022-05-12 15:21:01 +0400

    tmp

--- src/Mediator/Pipes/PipeConnectorExtensions.cs
+++ src/Mediator/Pipes/PipeConnectorExtensions.cs
@@ -28,30 +28,33 @@ namespace Mediator.Pipes;
 
 public static class PipeConnectorExtensions
 {
-    public static Task<PipeConnection> ConnectOutAsync<TMessage>(this IPipeConnector pipeConnector,
-        Func<IServiceProvider, IHandler<TMessage>> factory, string routingKey = "",
-        string subscriptionId = "") =>
-        pipeConnector.ConnectOutAsync<TMessage>(new HandlingPipe<TMessage>(factory), routingKey, subscriptionId);
+	public static Task<PipeConnection> ConnectOutAsync<TMessage>(this IPipeConnector pipeConnector,
+		Func<IServiceProvider, IHandler<TMessage>> factory, string routingKey = "",
+		string subscriptionId = "", CancellationToken token = default) =>
+		pipeConnector.ConnectOutAsync<TMessage>(new HandlingPipe<TMessage>(factory), routingKey, subscriptionId, token);
 
-    public static Task<PipeConnection> ConnectOutAsync<TMessage, TResult>(this IPipeConnector pipeConnector,
-        Func<IServiceProvider, IHandler<TMessage, TResult>> factory, string routingKey = "") =>
-        pipeConnector.ConnectOutAsync<TMessage, TResult>(new HandlingPipe<TMessage, TResult>(factory), routingKey);
+	public static Task<PipeConnection> ConnectOutAsync<TMessage, TResult>(this IPipeConnector pipeConnector,
+		Func<IServiceProvider, IHandler<TMessage, TResult>> factory, string routingKey = "",
+		CancellationToken token = default) =>
+		pipeConnector.ConnectOutAsync<TMessage, TResult>(new HandlingPipe<TMessage, TResult>(factory), routingKey, token);
 
-    public static Task<PipeConnection> ConnectOutAsync<TMessage>(this IPipeConnector pipeConnector,
-        IHandler<TMessage> handler, string routingKey = "", string subscriptionId = "") =>
-        pipeConnector.ConnectOutAsync(_ => handler, routingKey, subscriptionId);
+	public static Task<PipeConnection> ConnectOutAsync<TMessage>(this IPipeConnector pipeConnector,
+		IHandler<TMessage> handler, string routingKey = "", string subscriptionId = "",
+		CancellationToken token = default) =>
+		pipeConnector.ConnectOutAsync(_ => handler, routingKey, subscriptionId, token: token);
 
-    public static Task<PipeConnection> ConnectOutAsync<TMessage, TResult>(this IPipeConnector pipeConnector,
-        IHandler<TMessage, TResult> handler, string routingKey = "") =>
-        pipeConnector.ConnectOutAsync(_ => handler, routingKey);
+	public static Task<PipeConnection> ConnectOutAsync<TMessage, TResult>(this IPipeConnector pipeConnector,
+		IHandler<TMessage, TResult> handler, string routingKey = "",
+		CancellationToken token = default) =>
+		pipeConnector.ConnectOutAsync(_ => handler, routingKey, token: token);
 
-    public static Task<PipeConnection> ConnectOutAsync<TMessage, THandler>(this IPipeConnector pipeConnector,
-        string routingKey = "", string subscriptionId = "")
-        where THandler : IHandler<TMessage> =>
-        pipeConnector.ConnectOutAsync(p => p.GetRequiredService<THandler>(), routingKey, subscriptionId);
+	public static Task<PipeConnection> ConnectOutAsync<TMessage, THandler>(this IPipeConnector pipeConnector,
+		string routingKey = "", string subscriptionId = "", CancellationToken token = default)
+		where THandler : IHandler<TMessage> =>
+		pipeConnector.ConnectOutAsync(p => p.GetRequiredService<THandler>(), routingKey, subscriptionId, token: token);
 
-    public static Task<PipeConnection> ConnectOutAsync<TMessage, TResult, THandler>(this IPipeConnector pipeConnector,
-        string routingKey = "")
-        where THandler : IHandler<TMessage, TResult> =>
-        pipeConnector.ConnectOutAsync(p => p.GetRequiredService<THandler>(), routingKey);
+	public static Task<PipeConnection> ConnectOutAsync<TMessage, TResult, THandler>(this IPipeConnector pipeConnector,
+		string routingKey = "", CancellationToken token = default)
+		where THandler : IHandler<TMessage, TResult> =>
+		pipeConnector.ConnectOutAsync(p => p.GetRequiredService<THandler>(), routingKey, token: token);
 }
--- src/Mediator/Void.cs
+++ src/Mediator/Void.cs
@@ -21,15 +21,6 @@
 // OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 // SOFTWARE.
 
-namespace Mediator.Pipes;
+namespace Mediator;
 
-public static class PipeExtensions
-{
-    public static Task<PipeConnection> ConnectInAsync<TMessage>(this IPipe pipe, IPipeConnector pipeConnector,
-        string routingKey = "", string subscriptionId = "", CancellationToken token = default) =>
-        pipeConnector.ConnectOutAsync<TMessage>(pipe, routingKey, subscriptionId, token);
-
-    public static Task<PipeConnection> ConnectInAsync<TMessage, TResult>(this IPipe pipe, IPipeConnector pipeConnector,
-        string routingKey = "", CancellationToken token = default) =>
-        pipeConnector.ConnectOutAsync<TMessage, TResult>(pipe, routingKey, token);
-}
+public readonly record struct Void;
