780e705593fa56cf4622f0eed061e7ad87e7aea9
apolitov <zxfess@yandex.ru>
2022-05-14 00:56:58 +0400

    tmp

--- src/Mediator/IMediator.cs
+++ src/Mediator/IMediator.cs
@@ -5,9 +5,9 @@ namespace Mediator;
 
 public interface IMediator : IPipeConnector
 {
-    Task<TResult> SendAsync<TMessage, TResult>(TMessage message, Action<MessageContext>? contextBuilder = null,
+    Task PublishAsync<TMessage>(TMessage message, Action<MessageContext>? contextBuilder = null,
         CancellationToken token = default);
 
-    Task PublishAsync<TMessage>(TMessage message, Action<MessageContext>? contextBuilder = null,
+    Task<TResult> SendAsync<TMessage, TResult>(TMessage message, Action<MessageContext>? contextBuilder = null,
         CancellationToken token = default);
 }
