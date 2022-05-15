ff7a46fccca4b32e0695f930b72170d8f89c0ca1
apolitov <zxfess@yandex.ru>
2022-05-16 02:45:10 +0400

    tmp

--- src/Mediator/Pipes/Route.cs
+++ src/Mediator/Pipes/Route.cs
@@ -1,9 +1,9 @@
 namespace Mediator.Pipes;
 
-public readonly record struct Route(string MessageType, string RoutingKey = "", string? ResultType = null)
+public readonly record struct Route(string MessageType, string RoutingKey = "", string ResultType = "")
 {
     public Route(Type messageType, string routingKey = "", Type? resultType = null)
-        : this(messageType.FullName ?? messageType.Name, routingKey, resultType?.FullName ?? resultType?.Name)
+        : this(messageType.FullName ?? messageType.Name, routingKey, resultType?.FullName ?? resultType?.Name ?? "")
     {
     }
 
@@ -15,6 +15,5 @@ public readonly record struct Route(string MessageType, string RoutingKey = "", 
 
     public static implicit operator string(Route route) => route.ToString();
 
-    public override string ToString() =>
-        $"{MessageType}:{RoutingKey}{(ResultType is not null ? $":{ResultType}" : string.Empty)}";
+    public override string ToString() => $"{MessageType}:{RoutingKey}:{ResultType}";
 }
