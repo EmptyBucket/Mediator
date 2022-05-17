54c03af78a89cfd426d38f980758a081ef0e3dec
apolitov <zxfess@yandex.ru>
2022-05-17 21:54:30 +0400

    tmp

--- src/Mediator/Configurations/PipeBindsBuilderExtensions.cs
+++ src/Mediator/Configurations/PipeBindsBuilderExtensions.cs
@@ -34,11 +34,11 @@ public static class PipeBindsBuilderExtensions
     public static IPipeBinder Bind<TPipe>(this IPipeBinder pipeBinder, string pipeName = "") =>
         pipeBinder.Bind(typeof(TPipe), pipeName);
 
-    public static IPipeBinder BindInterfaces(this IPipeBinder pipeBinder, Type pipeType, string pipeName = "") =>
+    public static IPipeBinder BindInterfaces(this IPipeBinder pipeBinder, Type pipeType, string pipeName) =>
         pipeType.GetInterfaces()
             .Where(i => i.IsAssignableTo(typeof(IPubPipe)) || i.IsAssignableTo(typeof(IReqPipe)))
             .Aggregate(pipeBinder, (b, i) => b.Bind(i, pipeType, pipeName));
 
-    public static IPipeBinder BindInterfaces<TPipe>(this IPipeBinder pipeBinder, string pipeName = "") =>
+    public static IPipeBinder BindInterfaces<TPipe>(this IPipeBinder pipeBinder, string pipeName) =>
         pipeBinder.BindInterfaces(typeof(TPipe), pipeName);
 }
