b390485f85227008fc4cbba6ca0aee8146838b28
apolitov <zxfess@yandex.ru>
2022-06-01 01:07:28 +0400

    tmp

--- src/Mediator/MediatorTopology.cs
+++ src/Mediator/MediatorTopology.cs
@@ -4,10 +4,11 @@ namespace Mediator;
 
 public class MediatorTopology : IAsyncDisposable
 {
-    public MediatorTopology(IConnectingPipe dispatch, IConnectingPipe receive)
+    public MediatorTopology(IConnectingPipe dispatch, IConnectingPipe receive, IPipeFactory pipeFactory)
     {
         Dispatch = dispatch;
         Receive = receive;
+        PipeFactory = pipeFactory;
     }
 
     public void Deconstruct(out IConnectingPipe dispatch, out IConnectingPipe receive)
@@ -15,11 +16,20 @@ public class MediatorTopology : IAsyncDisposable
         dispatch = Dispatch;
         receive = Receive;
     }
+    
+    public void Deconstruct(out IConnectingPipe dispatch, out IConnectingPipe receive, out IPipeFactory pipeFactory)
+    {
+        dispatch = Dispatch;
+        receive = Receive;
+        pipeFactory = PipeFactory;
+    }
 
     public IConnectingPipe Dispatch { get; set; }
 
     public IConnectingPipe Receive { get; set; }
     
+    public IPipeFactory PipeFactory { get; }
+
     public async ValueTask DisposeAsync()
     {
         await Dispatch.DisposeAsync();
